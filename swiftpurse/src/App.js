2e759e7e627cbc4861156ac557cb0f31967c74e2
poswalsameer <poswalsameer2003@gmail.com>
2024-01-04 00:34:30 +0530

    Made the basic skeleton

---
+++ swiftpurse/src/App.js
@@ -0,0 +1,36 @@
+
+import './App.css';
+import './index.css';
+
+function App() {
+  return (
+    <>
+
+        <div className=' flex flex-col justify-center items-center' >
+
+            <h1 className=' m-20 text-8xl text-green-300 font-extrabold' >SWIFT-PURSE</h1>
+
+            <div className=' m-10 h-72 w-[40rem] flex flex-col justify-center items-center bg-red-200 rounded-lg' >
+
+              <div className='m-5 h-20 w-[35rem] bg-blue-600 rounded-lg ' >
+
+              </div>
+
+              <div className=' m-5 h-20 w-[35rem] bg-blue-600 rounded-lg ' >
+ 
+              </div>
+
+
+            </div>
+
+
+
+
+        </div>
+
+
+    </>
+  );
+}
+
+export default App;
