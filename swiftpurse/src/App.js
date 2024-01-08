472750d4a890efbe31ecc12b324b6e0d9a1e5184
poswalsameer <poswalsameer2003@gmail.com>
2024-01-08 22:09:42 +0530

    Skeleton is ready, moving onto work on props and hooks

--- swiftpurse/src/App.js
+++ swiftpurse/src/App.js
@@ -2,6 +2,7 @@
 
 import './App.css';
 import './index.css';
+import CustomBox from './Components/CustomBox.js';
 
 // import useConverter from './CustomHooks/useConverter';
 
@@ -16,7 +17,13 @@ function App() {
 
         <div className=' flex flex-col justify-center items-center' >
 
-            <h1 className=' m-20 text-8xl text-green-300 font-extrabold' >SWIFT-PURSE</h1>                
+            <h1 className=' m-20 text-8xl text-green-300 font-extrabold' >SWIFT-PURSE</h1>
+
+            <CustomBox />  
+
+            <button className='h-14 w-36 bg-blue-400 rounded-lg my-10 text-center text-black font-extrabold ' >SWAP</button> 
+
+            <CustomBox />             
 
         </div>
 
--- swiftpurse/src/Components/CustomBox.js
+++ swiftpurse/src/Components/CustomBox.js
@@ -4,7 +4,24 @@ function CustomBox() {
   return (
     <>
     
-        
+        <div className='h-28 w-[36rem] bg-blue-400 rounded-lg flex flex-col justify-center' >
+
+          <div className='flex flex-row justify-between my-2 ' >
+                <label className='text-black font-bold ml-6 ' > FROM </label>
+                <p className='text-black font-bold mr-4 ' >Currency Type</p>
+          </div>
+
+          <div className='flex flex-row justify-center my-2 ' >
+            <input type="number" className='h-8 w-[24rem] mr-10 rounded-lg ' />
+
+            <select className='ml-12 rounded-lg ' >
+              <option value="usd" >USD</option>
+            </select>
+          </div>
+
+          
+
+        </div>
     
     </>
   )
