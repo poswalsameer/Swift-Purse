4df555c97df003aa25db2be97076755640e3ff53
poswalsameer <poswalsameer2003@gmail.com>
2024-01-08 23:17:07 +0530

    Done with the first input box, options are not showing, something is wrong with the API acc to me

--- swiftpurse/src/App.js
+++ swiftpurse/src/App.js
@@ -3,14 +3,39 @@
 import './App.css';
 import './index.css';
 import CustomBox from './Components/CustomBox.js';
+import useConverter from './CustomHooks/useConverter';
+import { useState } from 'react';
 
 // import useConverter from './CustomHooks/useConverter';
 
 function App() {
 
-  // const currencyOptions = [];
-  
-  // let currency = "usd";
+  //USE STATE HOOKS TO MANAGE ALL THE STATES AND UI UPDATION FOR LABELS AND AMOUNT AND CURRENCY
+  const [amount, setAmount] = useState(0);
+  const [convertFrom, setConvertFrom] = useState('usd');
+  const [convertTo, setConvertTo] = useState('inr');
+  const [convertedAmount, setConvertedAmount] = useState(0);
+
+  //GETTING THE INFORMATION OF CURRENCY FROM THE CUSTOM HOOK CREATED
+  const currentCurrencyInfo = useConverter(convertFrom);
+
+  //GETTING ALL THE OPTIONS BY LOOPING THROUGH THE ARRAY OF OBJECTS FROM SELECTED CURRENCY
+  const currencyOptions = Object.keys(currentCurrencyInfo); 
+
+  //THIS FUNCTION DOES THE TASK OF CONVERTING THE CURRENCY
+  //IT DOES BY GETTING THE AMOUNT ENTERED BY THE USER AND THEN MULTIPLYING IT
+  //BY THE VALUE OF THE CURRENCY CHOSEN IN THE "TO" COLUMN 
+  const convertCurrency = () => {
+      setConvertedAmount( amount * currencyOptions[convertTo] );
+  }
+
+  //FUNCTION FOR THE SWAP BUTTON THAT DOES A SIMPLE TASK OF SWAPPING THE VALUE FROM THE "FROM" to "TO" AND ALSO SWAPS THE VALUE IN THE RESPECTIVE INPUT FIELDS
+  const swapInput = () => {
+    setConvertFrom(convertTo);
+    setConvertTo(convertFrom);
+    setAmount(convertedAmount);
+    setConvertedAmount(amount);
+  }
 
   return (
     <>
@@ -19,7 +44,15 @@ function App() {
 
             <h1 className=' m-20 text-8xl text-green-300 font-extrabold' >SWIFT-PURSE</h1>
 
-            <CustomBox />  
+            <CustomBox 
+              label='FROM'
+              amount={amount}
+              currencyOptions={currencyOptions}
+              onCurrencyChange={ (currency) => setConvertFrom(currency) }
+              onAmountChange={ (amount) => setAmount(amount) }
+              selectedCurrency={convertFrom}
+            
+            />  
 
             <button className='h-14 w-36 bg-blue-400 rounded-lg my-10 text-center text-black font-extrabold ' >SWAP</button> 
 
--- swiftpurse/src/Components/CustomBox.js
+++ swiftpurse/src/Components/CustomBox.js
@@ -1,21 +1,39 @@
 import React from 'react'
 
-function CustomBox() {
+function CustomBox({
+
+  // THESE ARE THE PROPS WE ARE PASSING ON
+  label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyArr = [],
+  selectedCurrency = "usd",
+  disableAmount = false,
+  disableCurrency = false,
+}) {
   return (
     <>
     
         <div className='h-28 w-[36rem] bg-blue-400 rounded-lg flex flex-col justify-center' >
 
           <div className='flex flex-row justify-between my-2 ' >
-                <label className='text-black font-bold ml-6 ' > FROM </label>
+                <label className='text-black font-bold ml-6 ' > {label} </label>
                 <p className='text-black font-bold mr-4 ' >Currency Type</p>
           </div>
 
           <div className='flex flex-row justify-center my-2 ' >
-            <input type="number" className='h-8 w-[24rem] mr-10 rounded-lg ' />
+            <input type="number" className='h-8 w-[24rem] mr-10 rounded-lg text-black font-bold p-2 ' disabled={disableAmount} value={amount} onChange={ (e) => onAmountChange && onAmountChange( Number(e.target.value)) } />
+            {/* ON AMOUNT CHANGE IS THE FUNCTION THAT HANDLES THE INPUT FIELD WHENEVER AMOUNT IS CHANGED */}
 
-            <select className='ml-12 rounded-lg ' >
-              <option value="usd" >USD</option>
+            <select className='ml-12 rounded-lg text-black ' value={selectedCurrency} onChange = { (e) => onCurrencyChange && onCurrencyChange( (e.target.value)) } disabled={disableCurrency} >
+              {/* ON CURRENCY CHANGE IS SIMILAR TO ON AMOUNT CHANGE */}
+
+              {/* LOOPING THROUGH THE CURRENCY ARRAY TO DISPLAY ALL THE CURRENCY IN OPTIONS FIELD */}
+              { currencyArr.map( (currency) => (
+                <option key={currency} value={currency}>{currency}</option>
+              )) }
+
             </select>
           </div>
 
